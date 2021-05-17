9aa70e09dbb6685aa7f9e321a354c3ac7be1878d
kbmodhia <75643955+kbmodhia@users.noreply.github.com>
2021-05-17 10:39:02 +0530

    updae token
    update token

--- app/controllers/gitapis_controller.rb
+++ app/controllers/gitapis_controller.rb
@@ -7,7 +7,7 @@ class GitapisController < ApplicationController
         http.use_ssl = true
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         request = Net::HTTP::Get.new(url)
-        request["authorization"] = 'token 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'
+        request["authorization"] = 'token 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'
         request["content-type"] = 'application/json'
         response = http.request(request)
         @api_data = JSON.parse(response.read_body)
